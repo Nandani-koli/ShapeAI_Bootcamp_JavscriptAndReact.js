2a00a33a11a63571e3c1339a52e98e6890ee1ab5
Nandini-koli <70843941+Nandini-koli@users.noreply.github.com>
2021-06-05 19:56:27 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Bootcamp List</h1>
+        <p>
+          1) Javascript and React.js
+          <br />
+          2) Python and Machine Learning
+        </p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>
+          * No Pre-request
+          <br />* Learn Basic Javascript
+          <br />* Build React components
+          <br />* Hand-on-project
+        </p>
+      </div>
+      <div className="note">
+        <h1>Python and Machine Learning</h1>
+        <p>
+          * No Pre-request
+          <br />* Learn Basic Python
+          <br />* Concepts of ML
+          <br />* Hand-on-project
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Note;
